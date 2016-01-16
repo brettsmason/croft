3e239eef9f70b1c89dfc1c662b2d7b42035de821
Brett Mason <brettsmason@gmail.com>
2016-01-16 21:35:18 +0000

    Fixed search.php template error and adjusted subsidiary/footer styling.

--- search.php
+++ search.php
@@ -39,7 +39,7 @@ get_header(); ?>
 		endif; ?>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
+	</div><!-- #primary -->
 
 <?php
 hybrid_get_sidebar( 'primary' ); // Loads the sidebar/primary.php template.
