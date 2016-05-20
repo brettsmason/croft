c09a0bd51f4cfc3729dbec0ae5de0fca7dd380cc
Brett Mason <brettsmason@gmail.com>
2016-05-20 16:34:28 +0100

    Add vertical class to Foundation menu walker.

--- inc/utility.php
+++ inc/utility.php
@@ -6,7 +6,7 @@
 class Foundation_Menu_Walker extends Walker_Nav_Menu {
 	function start_lvl( &$output, $depth = 0, $args = Array() ) {
 		$indent = str_repeat( "\t", $depth );
-		$output .= "\n$indent<ul class=\"menu submenu\">\n";
+		$output .= "\n$indent<ul class=\"menu vertical submenu\">\n";
 	}
 }
 
