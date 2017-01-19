e0af8d327a7e32eafecfbf98c519600f221df635
Brett Mason <brettsmason@gmail.com>
2017-01-19 14:55:28 +0000

    Start work on new gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,196 +1,206 @@
 var $        = require('gulp-load-plugins')();
-var pkg      = require('./package.json');
 var gulp     = require('gulp');
-var rimraf   = require('rimraf');
-var sequence = require('run-sequence');
+var yaml     = require('js-yaml');
 
-// Browsers to target when prefixing CSS.
-var COMPATIBILITY = [
-  'last 2 versions',
-  'ie >= 9',
-  'Android >= 2.3'
-];
-
-var THEME = {
-  current: {
-    name: "Croft",
-    slug: "croft",
-    prefix: "croft_"
-  },
-  new: {
-    name: "Croft",   // Theme name
-    slug: "croft",   // Themes textdomain
-    prefix: "croft_" // Theme prefix - used for function names
-  }
+// Load our config.yml file
+function loadConfig() {
+  var ymlFile = fs.readFileSync('config.yml', 'utf8');
+  return yaml.load(ymlFile);
 }
 
-// File paths to various assets are defined here.
-var PATHS = {
-  hc: [
-    'bower_components/hybrid-core/**'
-  ],
-  sass: [
-    'bower_components/foundation-sites/scss',
-    'bower_components/motion-ui/src'
-  ],
-  javascript: [
-    // What-input
-    'bower_components/what-input/what-input.js',
+/**
+ * Handle errors and alert the user.
+ */
+function handleErrors() {
+  var args = Array.prototype.slice.call(arguments);
 
-    // Foundation Core and utils
-    'bower_components/foundation-sites/js/foundation.core.js',
-    'bower_components/foundation-sites/js/foundation.util.*.js',
+  notify.onError({
+    'title': 'Task Failed [<%= error.message %>',
+    'message': 'See console.',
+    'sound': 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
+  }).apply(this, args);
 
-    // Individual Foundation JS components
-    'bower_components/foundation-sites/js/foundation.abide.js',
-    'bower_components/foundation-sites/js/foundation.accordion.js',
-    'bower_components/foundation-sites/js/foundation.accordionMenu.js',
-    'bower_components/foundation-sites/js/foundation.drilldown.js',
-    'bower_components/foundation-sites/js/foundation.dropdown.js',
-    'bower_components/foundation-sites/js/foundation.dropdownMenu.js',
-    'bower_components/foundation-sites/js/foundation.equalizer.js',
-    'bower_components/foundation-sites/js/foundation.interchange.js',
-    'bower_components/foundation-sites/js/foundation.magellan.js',
-    'bower_components/foundation-sites/js/foundation.offcanvas.js',
-    'bower_components/foundation-sites/js/foundation.orbit.js',
-    'bower_components/foundation-sites/js/foundation.responsiveMenu.js',
-    'bower_components/foundation-sites/js/foundation.responsiveToggle.js',
-    'bower_components/foundation-sites/js/foundation.reveal.js',
-    'bower_components/foundation-sites/js/foundation.slider.js',
-    'bower_components/foundation-sites/js/foundation.sticky.js',
-    'bower_components/foundation-sites/js/foundation.tabs.js',
-    'bower_components/foundation-sites/js/foundation.toggler.js',
-    'bower_components/foundation-sites/js/foundation.tooltip.js',
+  gutil.beep(); // Beep 'sosumi' again
 
-    // Custom JS files
-    'assets/js/vendor/*.js',
-    'assets/js/theme/theme.js'
-  ]
-};
+  // Prevent the 'watch' task from stopping
+  this.emit('end');
+}
 
-// Delete the "dist" folder
-// This happens every time a build starts
-gulp.task('clean', function(done) {
-  rimraf('dist', done);
-});
+/**
+ * Delete style.css and style.min.css before we minify and optimize.
+ */
+gulp.task('clean:styles', function() {
+  return del(['style.css', 'style.min.css']);
+} );
 
-// Copy Hybrid Core from the bower directory
-gulp.task('copy', function() {
-  gulp.src(PATHS.hc)
-    .pipe(gulp.dest('inc/hybrid-core'));
-});
-
-// Compile main stylesheet into CSS
-gulp.task('sass', function() {
+/**
+ * Compile Sass.
+ *
+ * https://www.npmjs.com/package/gulp-sass
+ * https://www.npmjs.com/package/gulp-autoprefixer
+ */
+gulp.task('sass', ['clean:styles'], function() {
   return gulp.src('assets/scss/style.scss')
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      includePaths: PATHS.sass,
-      outputStyle: 'expanded',
-      indentType: 'tab',
-      indentWidth: 1
-    })
-    .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: COMPATIBILITY
-    }))
-    .pipe(gulp.dest('./'))
-    .pipe($.rename({suffix: '.min'}))
-    .pipe($.cleanCss())
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('./'))
+
+  // Deal with errors.
+  .pipe(plumber({'errorHandler': handleErrors}))
+
+  // Wrap tasks in a sourcemap.
+  .pipe(sourcemaps.init())
+
+  // Compile Sass using LibSass.
+  .pipe(sass({
+    'includePaths': [
+      'bower_components/foundation-sites/scss',
+      'bower_components/motion-ui/src'
+    ],
+    'errLogToConsole': true,
+    'outputStyle': 'expanded',
+    'indentType': 'tab',
+    'indentWidth': 1
+  }))
+
+  // Parse with Autoprefixer.
+  .pipe(autoprefixer({
+    'browsers': BROWSERS
+  }))
+
+  // Create sourcemap.
+  .pipe(sourcemaps.write('./'))
+
+  // Create style.css.
+  .pipe(gulp.dest('./'));
 });
 
-// Compile editor stylesheet into CSS
-gulp.task('sass:editor', function() {
-  return gulp.src('assets/scss/editor-style.scss')
-    .pipe($.sass({
-      includePaths: PATHS.sass,
-    })
-    .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: COMPATIBILITY
-    }))
-    .pipe($.rename({suffix: '.min'}))
-    .pipe($.cleanCss())
-    .pipe(gulp.dest('assets/css'))
+/**
+ * Minify and optimize style.css.
+ *
+ * https://www.npmjs.com/package/gulp-cssnano
+ */
+gulp.task('cssnano', ['sass'], function() {
+  return gulp.src('style.css')
+  .pipe( plumber({'errorHandler': handleErrors}))
+  .pipe( cssnano({
+    'safe': true // Use safe optimizations
+  }))
+  .pipe(rename('style.min.css'))
+  .pipe(gulp.dest('./'));
 });
 
-// Combine JavaScript into one file and create minified version
-gulp.task('javascript', function() {
-  return gulp.src(PATHS.javascript)
-    .pipe($.babel())
-    .pipe($.concat('main.js'))
-    .pipe(gulp.dest('assets/js'))
-    .pipe($.rename({suffix: '.min'}))
-    .pipe($.uglify({mangle: false}))
-    .pipe(gulp.dest('assets/js'))
+/**
+ * Delete the svg-icons.svg before we minify, concat.
+ */
+gulp.task('clean:icons', function() {
+  return del(['assets/img/svg-icons.svg']);
 });
 
-// Translates theme files and create a pot file
-gulp.task('translate', function() {
-  return gulp.src('./**/**.php')
-    .pipe($.sort())
-    .pipe($.wpPot({
-        domain: THEME.current.slug,
-        bugReport: pkg.bugReport,
-        lastTranslator: pkg.lastTranslator,
-        team: pkg.team
-    }))
-    .pipe(gulp.dest('./languages'));
+/**
+ * Minify, concatenate, and clean SVG icons.
+ *
+ * https://www.npmjs.com/package/gulp-svgmin
+ * https://www.npmjs.com/package/gulp-svgstore
+ * https://www.npmjs.com/package/gulp-cheerio
+ */
+gulp.task('svg', ['clean:icons'], function() {
+  return gulp.src(paths.icons)
+  .pipe( plumber({'errorHandler': handleErrors}))
+  .pipe(svgmin())
+  .pipe(rename({'prefix': 'icon-'}))
+  .pipe(svgstore({'inlineSvg': true}))
+  .pipe(cheerio({
+    'run': function ($, file) {
+      $('svg').attr('style', 'display:none');
+      $('[fill]').removeAttr('fill');
+      $('path').removeAttr('class');
+    },
+    'parserOptions': {'xmlMode': true}
+  }))
+  .pipe(gulp.dest('assets/img/'));
 });
 
-// Build the theme assets
-gulp.task('build', function(done) {
-  sequence(['sass', 'sass:editor', 'javascript'], done);
+/**
+ * Concatenate javascript files into one.
+ * https://www.npmjs.com/package/gulp-concat
+ */
+gulp.task('concat', function() {
+  return gulp.src(paths.customScripts)
+  .pipe(plumber({'errorHandler': handleErrors}))
+  .pipe(babel({ignore: ['what-input.js']}))
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest('assets/js'));
 });
 
-// Replaces all theme specific names with new ones
-gulp.task('renametheme', function() {
-  return gulp.src(['**/*', '!bower_components/**', '!node_modules/**'])
-    .pipe($.replace(THEME.current.name, THEME.new.name))
-    .pipe($.replace(THEME.current.slug, THEME.new.slug))
-    .pipe($.replace(THEME.current.prefix, THEME.new.prefix))
-    .pipe(gulp.dest('./'));
+ /**
+  * Minify compiled javascript after concatenated.
+  * https://www.npmjs.com/package/gulp-uglify
+  */
+gulp.task('uglify', ['concat'], function() {
+  return gulp.src(paths.scripts)
+  .pipe(rename({'suffix': '.min'}))
+  .pipe(uglify({
+    'mangle': false
+  }))
+  .pipe(gulp.dest('assets/js'));
 });
 
-gulp.task('dist', ['clean'], function () {
-  return gulp.src([
-    '**',
-    '!node_modules/',
-    '!node_modules/**',
-    '!bower_components/',
-    '!bower_components/**',
-    '!assets/scss/',
-    '!assets/scss/**',
-    '!assets/js/theme/',
-    '!assets/js/theme/**',
-    '!assets/js/vendor/',
-    '!assets/js/vendor/**',
-    '!dist/',
-    '!dist/**',
-    '!.git/**',
-    '!bower.json',
-    '!package.json',
-    '!.gitignore',
-    '!.gitmodules',
-    '!.tx/**',
-    '!**/gulpfile.js',
-    '!**/package.json',
-    '!**/*~',
-    '!*.map'
-    ])
-  .pipe(gulp.dest('dist/' + pkg.name + '/'));
+/**
+ * Delete the theme's .pot before we create a new one.
+ */
+gulp.task('clean:pot', function() {
+  return del(['languages/legion.pot']);
 });
 
-gulp.task('zip', function () {
-  return gulp.src('dist/' + pkg.name + '/**')
-    .pipe($.zip(pkg.name + '-' + pkg.version + '.zip'))
-    .pipe(gulp.dest('dist'));
+/**
+ * Scan the theme and create a POT file.
+ *
+ * https://www.npmjs.com/package/gulp-wp-pot
+ */
+gulp.task('wp-pot', ['clean:pot'], function() {
+  return gulp.src(paths.php)
+  .pipe(plumber({'errorHandler': handleErrors}))
+  .pipe(sort())
+  .pipe(wpPot({
+    'domain': 'croft',
+    'destFile': 'croft.pot',
+    'package': 'legion',
+    'bugReport': 'https://github.com/brettsmason/croft/issues',
+    'lastTranslator': 'Brett Mason <brettsmason@gmail.com>',
+    'team': 'Brett Mason <brettsmason@gmail.com>'
+  }))
+  .pipe(gulp.dest('languages/'));
 });
 
-// Build the theme, run the server, and watch for file changes
-gulp.task('default', ['build'], function() {
-  gulp.watch(['assets/scss/**/*.scss'], ['sass', 'sass:editor']);
-  gulp.watch(['assets/js/theme/**/*.js', 'assets/js/vendor/**/*.js'], ['javascript']);
+/**
+ * Process tasks and reload browsers on file changes.
+ *
+ * https://www.npmjs.com/package/browser-sync
+ */
+gulp.task('watch', function() {
+
+  // Kick off BrowserSync.
+  browserSync({
+    'open': false,
+    'injectChanges': true,
+    'proxy': paths.server,
+    'watchOptions': {
+      'debounceDelay': 1000
+    }
+  });
+
+  // Run tasks when files change.
+  gulp.watch(paths.icons, ['icons']);
+  gulp.watch(paths.sass, ['styles']);
+  gulp.watch(paths.customScripts, ['scripts']);
+  gulp.watch(paths.vendorScripts, ['scripts']);
+  gulp.watch(paths.php, ['markup']);
 });
+
+/**
+ * Create individual tasks.
+ */
+gulp.task('markup', browserSync.reload);
+gulp.task('i18n', ['wp-pot']);
+gulp.task('icons', ['svg']);
+gulp.task('scripts', ['uglify']);
+gulp.task('styles', ['cssnano']);
+gulp.task('default', ['i18n', 'icons', 'styles', 'scripts']);
