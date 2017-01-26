f3f4698269b70d632c9f4c9b9cc33c77ad8997da
Brett Mason <brettsmason@gmail.com>
2017-01-26 14:47:00 +0000

    Correct indenting.

--- single.php
+++ single.php
@@ -3,26 +3,26 @@
 	<div id="primary" class="content-area">
 		<main <?php hybrid_attr( 'content' ); ?>>
 
-		<?php
-		/* Start the Loop */
-		while ( have_posts() ) : the_post(); ?>
-
 			<?php
-			/*
-			 * Loads the content/singular/post_type.php template
-			 * (where post_type is the name of the post type).
-			 */
-			hybrid_get_content_template(); ?>
+			/* Start the Loop */
+			while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'components/loop-nav' ); ?>
+				<?php
+				/*
+				* Loads the content/singular/post_type.php template
+				* (where post_type is the name of the post type).
+				*/
+				hybrid_get_content_template(); ?>
 
-			<?php
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) : ?>
-				<?php comments_template(); ?>
-			<?php endif; ?>
+				<?php get_template_part( 'template-parts/pagination' ); ?>
 
-		<?php endwhile; // End of the loop. ?>
+				<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) : ?>
+					<?php comments_template(); ?>
+				<?php endif; ?>
+
+			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
