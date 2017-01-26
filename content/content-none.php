5c972ad6130ab8f9ca2ebfea3989f44e3a355816
Brett Mason <brettsmason@gmail.com>
2017-01-26 14:44:29 +0000

    Fix incorrect no content template

--- content/content-none.php
+++ content/content-none.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying a message that posts cannot be found.
+ * Template part used if no content was found (404 or search).
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  */
@@ -9,24 +9,20 @@
 
 <section class="entry no-results not-found">
 	<header class="entry-header">
-		<h1 <?php hybrid_attr( 'entry-title' ); ?>><?php the_title(); ?></h1>
+		<h1 <?php hybrid_attr( 'entry-title' ); ?>><?php esc_html_e( 'Nothing Found', 'croft' ); ?></h1>
 	</header><!-- .entry-header -->
 
 	<div <?php hybrid_attr( 'entry-content' ); ?>>
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+		<?php if ( is_search() ) : ?>
 
-			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'croft' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
-		<?php elseif ( is_search() ) : ?>
-
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'croft' ); ?></p>
-			<?php get_search_form(); ?>
+			<p><?php esc_html_e( 'Sorry, nothing matched your search terms. Please try again with some different keywords.', 'croft' ); ?></p>
 
 		<?php else : ?>
 
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'croft' ); ?></p>
-			<?php get_search_form(); ?>
+			<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'croft' ); ?></p>
 
 		<?php endif; ?>
+
+		<?php get_search_form(); ?>
 	</div><!-- .entry-content -->
 </section><!-- .no-results -->
