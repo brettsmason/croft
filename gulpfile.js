377486a6f351a4a67926c2e35769ea098c34fc7b
Brett Mason <brettsmason@gmail.com>
2017-03-07 16:20:26 +0000

    Complete rewrite of gulpfile. Need to add a dist task, package task and rename task as well as potentially add css injection to BrowserSync.

--- gulpfile.js
+++ gulpfile.js
@@ -1,235 +1,132 @@
-var $        = require('gulp-load-plugins')();
-var gulp     = require('gulp');
-var yaml     = require('js-yaml');
-var del  = require('del');
-var browserSync  = require('browser-sync');
-var reload       = browserSync.reload;
-var fs       = require('fs');
+'use strict';
+
+const $       = require('gulp-load-plugins')();
+const browser = require('browser-sync');
+const gulp    = require('gulp');
+const rimraf  = require('rimraf');
+const yaml    = require('js-yaml');
+const fs      = require('fs');
 
 // Load settings from config.yml
-const { URL, BROWSERS, PATHS, POT, THEME } = loadConfig();
+const { URL, AUTOPREFIXER, PATHS, POT, THEME } = loadConfig();
 
-// Load our config.yml file
 function loadConfig() {
-  var ymlFile = fs.readFileSync('config.yml', 'utf8');
+  let ymlFile = fs.readFileSync('config.yml', 'utf8');
   return yaml.load(ymlFile);
 }
 
-/**
- * Handle errors and alert the user.
- */
-function handleErrors() {
-  var args = Array.prototype.slice.call(arguments);
-
-  $.notify.onError({
-    'title': 'Task Failed [<%= error.message %>',
-    'message': 'See console.',
-    'sound': 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
-  }).apply(this, args);
-
-  $.util.beep(); // Beep 'sosumi' again
-
-  // Prevent the 'watch' task from stopping
-  this.emit('end');
+// Delete the "dist" folder
+// This happens every time a build starts
+function clean(done) {
+  rimraf(PATHS.dist, done);
 }
 
-/**
- * Delete style.css and style.min.css before we minify and optimize.
- */
-gulp.task('clean:styles', function() {
-  return del(['style.css', 'style.min.css', 'style.css.map', 'style.min.css.map']);
-});
-
-/**
- * Compile Sass.
- *
- * https://www.npmjs.com/package/gulp-sass
- * https://www.npmjs.com/package/gulp-autoprefixer
- */
-gulp.task('sass', function() {
+// Compile and minify the main theme stylesheet.
+function sass() {
   return gulp.src('assets/scss/style.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.plumber())
+    .pipe($.sass({
+      includePaths: PATHS.sass,
+      outputStyle: 'expanded',
+      indentType: 'tab',
+      indentWidth: 1
+    })
+    .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: AUTOPREFIXER
+    }))
+    .pipe(gulp.dest('./'))
+    .pipe($.cssnano())
+    .pipe($.rename({'suffix': '.min'}))
+    .pipe($.sourcemaps.write('./'))
+    .pipe(gulp.dest('./'));
+}
 
-  // Deal with errors.
-  .pipe($.plumber({'errorHandler': handleErrors}))
+// Compile and minify WordPress editor stylesheet.
+function sassEditor() {
+  return gulp.src('assets/scss/editor-style.scss')
+    .pipe($.plumber())
+    .pipe($.sass({
+      includePaths: PATHS.sass,
+      outputStyle: 'compressed'
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: AUTOPREFIXER
+    }))
+    .pipe($.rename({'suffix': '.min'}))
+    .pipe(gulp.dest('assets/css'));
+}
 
-  // Wrap tasks in a sourcemap.
-  .pipe($.sourcemaps.init())
+// Concatenate and minify JavaScript files.
+function javascript() {
+  return gulp.src(PATHS.javascript)
+    .pipe($.plumber())
+    .pipe($.babel({ignore: ['what-input.js']}))
+    .pipe($.concat('main.js'))
+    .pipe(gulp.dest('assets/js'))
+    .pipe($.rename({'suffix': '.min'}))
+    .pipe($.uglify({'mangle': false}))
+    .pipe(gulp.dest('assets/js'))
+    .pipe(gulp.dest(PATHS.dist + '/assets/js'));
+}
 
-  // Compile Sass using LibSass.
-  .pipe($.sass({
-    'includePaths': [
-      'node_modules/foundation-sites/scss',
-      'node_modules/motion-ui/src'
-    ],
-    'errLogToConsole': true,
-    'outputStyle': 'expanded',
-    'indentType': 'tab',
-    'indentWidth': 1
-  }))
-
-  // Parse with Autoprefixer.
-  .pipe($.autoprefixer({
-    'browsers': BROWSERS
-  }))
-
-  // Create sourcemap.
-  .pipe($.sourcemaps.write('./'))
-
-  // Create style.css.
-  .pipe(gulp.dest('./'));
-});
-
-/**
- * Minify and optimize style.css.
- *
- * https://www.npmjs.com/package/gulp-cssnano
- */
-gulp.task('cssnano', function() {
-  return gulp.src('style.css')
-  .pipe($.plumber({'errorHandler': handleErrors}))
-  
-  // Wrap tasks in a sourcemap.
-  .pipe($.sourcemaps.init())
-
-  .pipe($.cssnano({
-    'safe': true // Use safe optimizations
-  }))
-  .pipe($.rename('style.min.css'))
-
-  // Create sourcemap.
-  .pipe($.sourcemaps.write('./'))
-
-  .pipe(gulp.dest('./'));
-});
-
-/**
- * Delete the svg-icons.svg before we minify, concat.
- */
-gulp.task('clean:icons', function() {
-  return del(['assets/img/svg-icons.svg']);
-});
-
-/**
- * Minify, concatenate, and clean SVG icons.
- *
- * https://www.npmjs.com/package/gulp-svgmin
- * https://www.npmjs.com/package/gulp-svgstore
- * https://www.npmjs.com/package/gulp-cheerio
- */
-gulp.task('svg', function() {
+// Minify, concatenate, and clean SVG icons.
+function svg() {
   return gulp.src(PATHS.icons)
-  .pipe($.plumber({'errorHandler': handleErrors}))
+  .pipe($.plumber())
   .pipe($.svgmin())
   .pipe($.rename({'prefix': 'icon-'}))
   .pipe($.svgstore({'inlineSvg': true}))
   .pipe($.cheerio({
-    'run': function ($, file) {
+    run: function ($, file) {
       $('svg').attr('style', 'display:none');
       $('[fill]').removeAttr('fill');
       $('path').removeAttr('class');
     },
-    'parserOptions': {'xmlMode': true}
+    parserOptions: {'xmlMode': true}
   }))
-  .pipe(gulp.dest('assets/img/'));
-});
+  .pipe(gulp.dest('assets/img'));
+}
 
-/**
- * Delete main.js and main.min.js before we concat and uglify.
- */
-gulp.task('clean:scripts', function() {
-  return del(['assets/js/main.js', 'assets/js/*.min.js']);
-});
-
-/**
- * Concatenate javascript files into one.
- * https://www.npmjs.com/package/gulp-concat
- */
-gulp.task('concat', function() {
-  return gulp.src(PATHS.javascript)
-  .pipe($.plumber({'errorHandler': handleErrors}))
-  .pipe($.babel({ignore: ['what-input.js']}))
-  .pipe($.concat('main.js'))
-  .pipe(gulp.dest('assets/js'));
-});
-
- /**
-  * Minify compiled javascript after concatenated.
-  * https://www.npmjs.com/package/gulp-uglify
-  */
-gulp.task('uglify', function() {
-  return gulp.src('assets/js/*.js')
-  .pipe($.rename({'suffix': '.min'}))
-  .pipe($.uglify({
-    'mangle': false
-  }))
-  .pipe(gulp.dest('assets/js'));
-});
-
-/**
- * Delete the theme's .pot before we create a new one.
- */
-gulp.task('clean:pot', function() {
-  return del(['./languages/' + POT.domain + '.pot']);
-});
-
-/**
- * Scan the theme and create a POT file.
- *
- * https://www.npmjs.com/package/gulp-wp-pot
- */
-gulp.task('wp-pot', function() {
+// Scan the theme and create a POT file.
+function translate() {
   return gulp.src(PATHS.php)
-  .pipe($.plumber({'errorHandler': handleErrors}))
-  .pipe($.sort())
-  .pipe($.wpPot({
-    'domain': POT.domain,
-    'package': POT.package
-  }))
-  .pipe(gulp.dest('./languages/' + POT.domain + '.pot'));
-});
+    .pipe($.plumber())
+    .pipe($.sort())
+    .pipe($.wpPot({
+      domain: POT.domain,
+      package: POT.package
+    }))
+    .pipe(gulp.dest('languages/' + POT.domain + '.pot'));
+}
 
-/**
- * Process tasks and reload browsers on file changes.
- *
- * https://www.npmjs.com/package/browser-sync
- */
-gulp.task('watch', function() {
-
-  // Kick off BrowserSync.
-  browserSync({
-    'open': false,
-    'injectChanges': true,
-    'proxy': URL,
-    'watchOptions': {
-      'debounceDelay': 1000
-    }
+// Start a server with BrowserSync to preview the site in.
+function server(done) {
+  browser.init({
+    proxy: URL,
+    injectChanges: true
   });
+  done();
+}
 
-  // Run tasks when files change.
-  gulp.watch(PATHS.icons, gulp.parallel('icons'));
-  gulp.watch('assets/scss/**/*.scss', gulp.parallel('styles'));
-  gulp.watch(PATHS.javascript, gulp.parallel('scripts'));
-  gulp.watch(PATHS.php, gulp.parallel('markup'));
-});
+// Reload the browser with BrowserSync.
+function reload(done) {
+  browser.reload();
+  done();
+}
 
-/**
- *  Rename all theme specific strings with new ones.
- *  These are set in config.yml.
- */
-gulp.task('rename', function() {
-  return gulp.src(['**/*', '!inc/hybrid-core/**', '!node_modules/**'])
-    .pipe($.replace(THEME.CURRENT.name, THEME.NEW.name))
-    .pipe($.replace(THEME.CURRENT.slug, THEME.NEW.slug))
-    .pipe($.replace(THEME.CURRENT.prefix, THEME.NEW.prefix))
-    .pipe(gulp.dest('./'));
-});
+// Watch for changes to assets and php files.
+function watch() {
+  gulp.watch('assets/scss/**/*.scss').on('all', gulp.series(gulp.parallel(sass)));
+  gulp.watch('assets/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
+  gulp.watch('assets/img/**/*.svg').on('all', gulp.series(svg, browser.reload));
+  gulp.watch('**/*.php').on('all', gulp.series(browser.reload));
+}
 
-/**
- * Create individual tasks.
- */
-gulp.task('markup', browserSync.reload);
-gulp.task('i18n', gulp.series('clean:pot', 'wp-pot'));
-gulp.task('icons', gulp.series('clean:icons', 'svg'));
-gulp.task('scripts', gulp.series('clean:scripts', 'concat', 'uglify'));
-gulp.task('styles', gulp.series('clean:styles', 'sass', 'cssnano'));
-gulp.task('default', gulp.parallel('i18n', 'icons', 'styles', 'scripts'));
+// Build the assets by running all of the below tasks
+gulp.task('build', gulp.parallel(sass, sassEditor, javascript, svg, translate));
+
+// Build the assets, run the server, and watch for file changes
+gulp.task('default', gulp.series('build', server, watch));
