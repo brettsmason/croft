f0ca65759d0dc7be122789fe0110e70505f46351
Brett Mason <brettsmason@gmail.com>
2017-07-06 15:37:16 +0100

    Update pagination

--- partials/pagination.php
+++ partials/pagination.php
@@ -13,19 +13,22 @@
 
 <?php if ( is_singular( 'post' ) ) : // If viewing a single post page. ?>
 
-	<div class="pager">
-		<?php previous_post_link( '<div class="prev">' . esc_html__( 'Previous: %link', 'croft' ) . '</div>', '%title' ); ?>
-		<?php next_post_link(     '<div class="next">' . esc_html__( 'Next: %link',     'croft' ) . '</div>', '%title' ); ?>
-	</div><!-- .loop-nav -->
+	<nav class="pager-container">
+		<ul class="pager" role="navigation" aria-label="Pagination">
+			<?php previous_post_link( '<li class="pager-item pager-prev"><span class="pager-item-title">' . esc_html_x( 'Previous:', 'previous post navigation', 'croft' ) . '</span>%link</li>' ); ?>
+			<?php next_post_link( '<li class="pager-item pager-next"><span class="pager-item-title">' . esc_html_x( 'Next:', 'next post navigation', 'croft' ) . '</span>%link</li>' ); ?>
+		</ul>
+	</nav><!-- .pager-container -->
 
 <?php elseif ( hybrid_is_plural() ) : // If viewing the blog, an archive, or search results. ?>
 
 	<?php the_posts_pagination(
 		array(
-			'prev_text' => esc_html_x( 'Previous', 'posts navigation', 'croft' ),
-			'next_text' => esc_html_x( 'Next',     'posts navigation', 'croft' ),
+			'mid_size'           => 2,
+			'prev_text'          => esc_html_x( 'Previous', 'posts navigation', 'croft' ),
+			'next_text'          => esc_html_x( 'Next',     'posts navigation', 'croft' ),
 			'before_page_number' => '<span class="screen-reader-text">' . __( 'Page', 'croft' ) . ' </span>',
-			'type' => 'list'
+			'type'               => 'list'
 		)
 	); ?>
 
